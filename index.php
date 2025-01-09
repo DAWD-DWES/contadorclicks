8e4f2787e471ca81e40fbdd846e2182052025a1e
jivanrod <jivanrod@gmail.com>
2025-01-09 16:44:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+            <h1>El número de clicks hasta ahora es: </h1>
+            <input type="submit" value="Click" name="click">  
+        </form>
+    </body>
+</html>
